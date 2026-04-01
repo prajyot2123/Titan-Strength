a6612986459f428cce0dd1c77e03543bc5cef0ca
Prajyot Deshpande <127429853+prajyot2123@users.noreply.github.com>
2026-04-01 15:30:58 +0530

    Add files via upload

---
+++ server/routes/index.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+const path = require("path");
+const apiRoutes = require("./api");
+
+router.use("/api", apiRoutes);
+
+// connect with react router
+// serve up react front-end in production
+router.use((req, res) => {
+  const buildPath = path.join(__dirname, "../../client/build/index.html");
+  if (process.env.NODE_ENV === "production") {
+    return res.sendFile(buildPath, (err) => {
+      if (err) {
+        return res.status(404).json({ message: "Client build not found." });
+      }
+      return undefined;
+    });
+  }
+  return res.status(404).json({ message: "Route not found." });
+});
+
+module.exports = router;
